60541e173bb8118cf8397bc93c6c876f00925643
elena_dobruk <elena.dobruk@yandex.ru>
2025-03-20 12:45:12 +0300

    add artciles models and make migrations

--- drfsite/articles/models.py
+++ drfsite/articles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
