8995f15017d46328dc5958cd18f5bb491eb6e3db
elena_dobruk <elena.dobruk@yandex.ru>
2025-04-07 14:56:37 +0300

    update up to lesson 4

--- drfsite/articles/serializers.py
+++ drfsite/articles/serializers.py
@@ -1,9 +1,57 @@
+# import io
+
 from rest_framework import serializers
+# from rest_framework.parsers import JSONParser
+# from rest_framework.renderers import JSONRenderer
 
-from .models import Article
+# from .models import Article
 
 
-class ArticlesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = ('name', 'body', 'category')
+# Lesson 4-2.
+# Practice.
+class ArticlesSerializer(serializers.Serializer):
+    """From .models - Article."""
+    name = serializers.CharField(max_length=250)
+    body = serializers.CharField()
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+    category_id = serializers.IntegerField()
+
+# ---------------------------------------------------------------------
+# Lesson 4-1.
+# Theory.
+# class ArticlesModel:
+#     def __init__(self, name, body):
+#         self.name = name
+#         self.body = body
+#
+#
+# class ArticlesSerializer(serializers.Serializer):
+#     name = serializers.CharField(max_length=255)  # должно совпасть с self
+#     body = serializers.CharField()  # должно совпасть с названием в self
+#
+#
+# def encode():
+#     """ Кодируем преобразование объектов ArticlesModel
+#     в JSON-format."""
+#     model = ArticlesModel('Aloy', 'Content: Aloy Ahoy!')
+#     model_sr = ArticlesSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+#
+#
+# def decode():
+#     stream = io.BytesIO(b'{"name":"Aloy","body":"Content: Aloy Ahoy!"}')  # имитируем клиента
+#     data  = JSONParser().parse(stream)
+#     serializer = ArticlesSerializer(data=data)  # декодируем данные
+#     serializer.is_valid()
+#     print(serializer.validated_data)  # вернет объект-словарь
+#
+# ----------------------------------------------------------------------------
+# Lesson 2.
+# A serializer example.
+# class ArticlesSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Article
+#         fields = ('name', 'body', 'category')
--- drfsite/articles/views.py
+++ drfsite/articles/views.py
@@ -1,9 +1,47 @@
-from rest_framework import generics
+# from django.forms import model_to_dict
+# from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Article
 from .serializers import ArticlesSerializer
 
 
-class ArticlesAPIView(generics.ListAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticlesSerializer
+# Lesson 4.
+class ArticlesAPIView(APIView):
+    def get(self, request):
+        lst = Article.objects.all()
+        return Response({'posts': ArticlesSerializer(lst, many=True).data})  # список записей, поэтому many
+    
+    def post(self, request):
+        serializer = ArticlesSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        post_new = Article.objects.create(
+            name=request.data['name'],
+            body=request.data['body'],
+            category_id=request.data['category_id'],
+        )
+        return Response({'post': ArticlesSerializer(post_new).data})
+
+# Lesson 3.
+# Учим базовый класс вьюх - APIView: без сериализаторов.
+# class ArticlesAPIView(APIView):
+#     def get(self, request):
+#         lst = Article.objects.all().values()
+#         return Response({'posts': list(lst)})
+    
+#     def post(self, request):
+#         post_new = Article.objects.create(
+#             name=request.data['name'],
+#             body=request.data['body'],
+#             category_id=request.data['category_id'],
+#         )
+#         return Response({'post': model_to_dict(post_new)})
+#
+# ------------------------------------------------------------------------
+# Lesson 2.
+# Смотрим пример того, как можно быстро писать вьюхи.
+# class ArticlesAPIView(generics.ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticlesSerializer
