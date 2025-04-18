cefce033b5c9a6af857df61a1f8f30b0c476a49a
elena_dobruk <elena.dobruk@yandex.ru>
2025-04-18 13:05:11 +0300

    update flask-practice with lesson 7

--- flask-practice/app.py
+++ flask-practice/app.py
@@ -1,9 +1,12 @@
 from flask import (
     Flask,
+    abort,
     flash,
+    redirect,
     render_template,
     request,
-    # url_for,
+    session,
+    url_for,
 )
 
 app = Flask(__name__)
@@ -21,9 +24,23 @@ def about():
     return render_template('about.html', title='About')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'owl' and request.form['password'] == '123':
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+    
+    return render_template('login.html', title='Login Time')
+
+
 @app.route('/profile/<path:username>')  # конвертеры для обозначения пути (int=только цифры, path=любые допустимые символы и слэш и тд)
 def profile(username):
-    return f'Current user: {username}'
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+
+    return render_template('profile.html', title='Your Profile', username=username)
 
 
 @app.route('/contact', methods=['GET', 'POST'])
@@ -40,9 +57,14 @@ def contact():
     return render_template('contact.html', title='Contact Us')
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('not_found.html', title='Got Lost?'), 404
+
 # with app.test_request_context():
 #     print(url_for('about'))
 #     print(url_for('profile', username='Wyll'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)  # для тестов в dev-среде
