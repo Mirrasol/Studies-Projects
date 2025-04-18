5dd67410f47fea611c610e5981e5d5a905271110
elena_dobruk <elena.dobruk@yandex.ru>
2025-04-18 16:01:14 +0300

    update flask-practice with lessons 22-23, SQLAlchemy

---
+++ flask-practice/app_old.py
@@ -0,0 +1,70 @@
+from flask import (
+    Flask,
+    abort,
+    flash,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'veryverysecret'
+
+
+@app.route('/homepage')
+@app.route('/')
+def homepage():
+    return render_template('homepage.html', title='Home')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'owl' and request.form['password'] == '123':
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+    
+    return render_template('login.html', title='Login Time')
+
+
+@app.route('/profile/<path:username>')  # конвертеры для обозначения пути (int=только цифры, path=любые допустимые символы и слэш и тд)
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+
+    return render_template('profile.html', title='Your Profile', username=username)
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        if '@' not in request.form['email']:
+            flash('Invalid email', category='danger')
+        else:
+            flash('Email has been sent!', category='success')
+
+        print(request.form)
+        print(request.form['email'])
+        
+    return render_template('contact.html', title='Contact Us')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('not_found.html', title='Got Lost?'), 404
+
+# with app.test_request_context():
+#     print(url_for('about'))
+#     print(url_for('profile', username='Wyll'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)  # для тестов в dev-среде
