0201f4963927794b3c0b0cab5fe0f58db30839e1
elena_dobruk <elena.dobruk@yandex.ru>
2025-04-25 16:19:55 +0300

    update avito-testing: add create endpoint

--- avito-testing/api/app.py
+++ avito-testing/api/app.py
@@ -1,8 +1,15 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, request
+# from flask_sqlalchemy import SQLAlchemy
+from pydantic import ValidationError
+
+from api.data import CreateAdvertismentDTO
+from api.db import db
+
+# from api.models import AdvertisementModel
+from api.service import create_advertisment_service
 
 load_dotenv()
 
@@ -10,7 +17,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 
-db = SQLAlchemy(app)
+db.init_app(app)
+
 
 with app.app_context():
     db.create_all()
@@ -21,12 +29,35 @@ def index():
     return '<p>Advertisments!</p>'
 
 
-@app.route('/advertisments')
+@app.post('/advertisments')
 def create_advertisment():
+    body = request.json
+    body['photos'] = ", ".join(body['photos'])
+
+    try:
+        body = CreateAdvertismentDTO(**body)
+    except ValidationError as e:
+        return jsonify(e.errors()), 400
+
+    return jsonify(dict(create_advertisment_service(db, body)))
+
+
+@app.get('/advertisments')
+def get_advertisments():
     pass
 
 
-if __name__ == "__main__":
+@app.get('/advertisments/search')
+def search_advertisments():
+    pass
+
+
+def main():
     host = os.getenv('HOST')
     port = int(os.getenv('PORT'))
+    app.config['DEBUG'] = os.getenv('DEBUG')
     app.run(host, port)
+
+
+if __name__ == "__main__":
+    main()
