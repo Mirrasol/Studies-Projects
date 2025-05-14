d5e6d188c6b84b0c7be4b78d5455ae3a135e0f53
elena_dobruk <elena.dobruk@yandex.ru>
2025-05-14 17:29:28 +0300

    update web-parsing with sessions, p.2

--- web-parsing/sessions.py
+++ web-parsing/sessions.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from itertools import cycle
 
 # 1. Session Example
 session = requests.Session()
@@ -14,12 +15,52 @@ session.close()
 
 
 # 2. Замеряем время сессии
+with requests.Session() as session:
+    start_time = time.time()
+    for _ in range(10):
+        response = session.get('https://example.com')
+    end_time = time.time()
+
+    print(f'Время выполнения с переиспользованием соединения: {end_time - start_time}')
+
+
+# 3. Аутентификация на прокси
+url = "https://httpbin.org/ip"
+
+proxies = {
+    'http': 'socks5://8ZYk5H:XfMpg7@10.10.36.159:8000',
+    'https': 'socks5://Kx4Jcj:h4Ch0N@10.10.51.205:8000',
+}
+
 session = requests.Session()
 
-# Измерение времени выполнения запросов с переиспользованием соединения
-start_time = time.time()
-for _ in range(10):
-    response = session.get('https://example.com')
-end_time = time.time()
+session.proxies.update(proxies)
 
-print(f'Время выполнения с переиспользованием соединения: {end_time - start_time}')
+response = session.get(url)
+
+print(response.text)
+
+
+# 4. Автоматом переключаем прокси
+proxies_list = [
+    {'http': 'http://10.10.36.159:8000', 'https': 'https://10.10.36.159:8000'},
+    {'http': 'http://10.10.51.205:8000', 'https': 'https://10.10.51.205:8000'},
+    {'http': 'http://10.10.79.216:8000', 'https': 'https://10.10.79.216:8000'},
+    # ... и так далее
+]
+
+# Создаём бесконечный итератор, который по кругу перебирает элементы из списка proxies_list:
+proxy_pool = cycle(proxies_list)
+
+url = "http://example.org"
+
+session = requests.Session()
+
+for i in range(1, 6):
+    proxy = next(proxy_pool)       # Берём следующий прокси из "пула", по кругу
+    session.proxies.update(proxy)  # Обновление прокси для сессии
+    try:
+        response = session.get(url, timeout=5)  # Используем сессию для выполнения запроса
+        print(f"Request {i}: Success!")
+    except requests.exceptions.RequestException as e:
+        print(f"Request {i}: Failed, switching proxy. {proxy}")
