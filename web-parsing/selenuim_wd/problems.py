89acbf7ec4566d36853b398629340a0905bbd803
elena_dobruk <elena.dobruk@yandex.ru>
2025-06-18 16:44:50 +0300

    update web-parsing with selenium methods problems

--- web-parsing/selenuim_wd/problems.py
+++ web-parsing/selenuim_wd/problems.py
@@ -2,7 +2,39 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-url = 'http://parsinger.ru/selenium/2/2.html'
+#1. Ищем число на рефрешах. 
+url = 'https://parsinger.ru/methods/1/index.html'
 
 with webdriver.Chrome() as browser:
     browser.get(url)
+    while True:
+        result = browser.find_element(By.ID, 'result').text
+        browser.refresh()
+        if result.lower() != 'refresh page':
+            print(result)
+            break
+
+#2. Scroll forth and back.
+url = 'https://parsinger.ru/selenium/6/6.2/index.html'
+
+with webdriver.Chrome() as browser:
+    browser.get(url)
+
+    browser.find_element('tag name', 'a').click()
+    browser.find_element('tag name', 'a').click()
+
+    browser.back()
+    browser.back()
+
+    button = browser.find_element('tag name', 'button').click()
+    time.sleep(5)
+
+# 3. Скриншот
+url = 'https://parsinger.ru/selenium/6/6.2.1/index.html'
+
+with webdriver.Chrome() as browser:
+    browser.get(url)
+    pic = browser.find_element('id', 'this_pic').screenshot('pic.png')
+
+# 4. ActionChains
+url = 'https://parsinger.ru/selenium/5.6/3/index.html'
