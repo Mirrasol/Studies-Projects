0023516829ad50c456a1349c00b9f9110bc83d6e
elena_dobruk <elena.dobruk@yandex.ru>
2025-07-02 12:21:42 +0300

    update web-parsing with more selenium practice

--- web-parsing/selenuim_wd/actions.py
+++ web-parsing/selenuim_wd/actions.py
@@ -2,7 +2,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.options import Options
 
 #1. Scrolling
 
@@ -21,3 +21,33 @@ with webdriver.Chrome() as browser:
         actions.scroll_by_amount(0, 500).perform()
         time.sleep(5)
 
+#2. Drag and Drop
+url = "https://parsinger.ru/selenium/5.10/1/index.html"
+
+with webdriver.Chrome() as browser:
+    browser.get(url)
+
+    draganddrop = browser.find_element(By.CLASS_NAME, "draganddrop")
+    draganddrop_end = browser.find_element(By.CLASS_NAME, "draganddrop_end")
+
+    ActionChains(browser).drag_and_drop(draganddrop, draganddrop_end).perform()
+    time.sleep(5)
+
+#3. Пример каптчи
+with webdriver.Chrome() as browser:
+    browser.get('https://parsinger.ru/selenium/5.10/5/index.html')
+
+    slider = browser.find_element(By.ID,'volume')
+    width = slider.size['width']
+
+    offset = width / 100
+
+    actions = ActionChains(browser)
+
+    actions.click_and_hold(slider).perform()
+
+    for _ in range(10):  # пример для 10 шагов
+        actions.move_by_offset(offset, 0).perform()
+        time.sleep(0.1)  # пауза для наглядности
+
+    actions.release().perform()
